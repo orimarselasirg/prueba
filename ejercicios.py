4d5851dbce8288c06fd20bb24a84e5b998c88f01
ramiro <ramirogrisales@gmail.com>
2025-11-06 18:36:39 -0500

    archivo ejercicio terminado

---
+++ ejercicios.py
@@ -0,0 +1,14 @@
+print("Hola, mundo")
+
+print("Bienvenido al programa de calculadora")
+
+print("Ingrese el primer numero:")
+numero1 = int(input())
+
+print("Ingrese el segundo numero:")
+numero2 = int(input())
+
+print("Ingrese la operacion:")
+operacion = input()
+
+print("El resultado es:")
